3892abb0850765b5cd0dc3e983180c06ae6a0993
SMT071 <SMT071@218.157.111.43>
2020-07-29 14:55:17 +0900

    주최측 삽입 삭제 수정 로그인 완료!

--- 페고파1차프로젝트/src/페고파1차프로젝트/HOST_DAO.java
+++ 페고파1차프로젝트/src/페고파1차프로젝트/HOST_DAO.java
@@ -6,11 +6,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HOST_DAO {
 	Connection conn = null;
 	PreparedStatement pst = null;
 	ResultSet rs = null;
+	Scanner sc = new Scanner(System.in);
 
 	private void getConn() {
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
@@ -46,31 +48,188 @@ public class HOST_DAO {
 		}
 	}
 
-	// 전체 가져오기
-	public ArrayList<HOST_VO> selectAll() {
-		ArrayList<HOST_VO> HOST = new ArrayList<HOST_VO>();
-
-		getConn();
-
+	//회원가입
+	public int insert(String iD, String pW, String 주소, String 기관명, String 연락처) {
+		
+		int cnt=0;
 		try {
-			String sql = "select * from HOST";
+			getConn();
+			
+			String sql = "insert into HOST_VO values(?,?,?,?,?)";
+			
 			pst = conn.prepareStatement(sql);
-			rs = pst.executeQuery();
-			while (rs.next()) {
-				String ID = rs.getString(1);
-				String PW = rs.getString(2);
-				String 주소 = rs.getString(3);
-				String 기관명 = rs.getString(4);
-				String 연락처 = rs.getString(5);
-				HOST.add(new HOST_VO(ID, PW, 주소, 기관명, 연락처));
+			
+			pst.setString(1, iD);
+			pst.setString(2, pW);
+			pst.setString(3, 주소);
+			pst.setString(4, 기관명);
+			pst.setString(5, 연락처);
+			
+			cnt = pst.executeUpdate();
+			
+			if(cnt>0) {
+				System.out.println("성공");
+			}else {
+				System.out.println("실패");
 			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			close();
 		}
-		return HOST;
-
+		return cnt;
 	}
+	//회원탈퇴
+	public int delete(String iD) { 
+
+	int cnt = 0;
+	try {
+		getConn();
+
+		String sql = "DELETE FROM HOST";
+
+		pst = conn.prepareStatement(sql);
+
+		cnt = pst.executeUpdate();
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		close();
+	}
+	return cnt;
+}	
+	//회원수정1번~5번까지
+	public int update1(String iD) {
+
+	int cnt = 0;
+	try {
+		getConn();
+
+		String sql = "UPDATE apply SET iD=?";
+
+		pst = conn.prepareStatement(sql);
+
+		pst.setString(1, iD);
+
+		cnt = pst.executeUpdate();
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		close();
+	}
+
+	return cnt;
+}	public int update2(String pW) {
+
+	int cnt = 0;
+	try {
+		getConn();
+
+		String sql = "UPDATE apply SET pW=?";
+
+		pst = conn.prepareStatement(sql);
+
+		pst.setString(1, pW);
+
+		cnt = pst.executeUpdate();
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		close();
+	}
+
+	return cnt;
+}	public int update3(String 주소) {
+
+	int cnt = 0;
+	try {
+		getConn();
+
+		String sql = "UPDATE apply SET 주소=?";
+
+		pst = conn.prepareStatement(sql);
+
+		pst.setString(1, 주소);
+
+		cnt = pst.executeUpdate();
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		close();
+	}
+
+	return cnt;
+}	public int update4(String 기관명) {
+
+	int cnt = 0;
+	try {
+		getConn();
+
+		String sql = "UPDATE apply SET 기관명=?";
+
+		pst = conn.prepareStatement(sql);
+
+		pst.setString(1, 기관명);
+
+		cnt = pst.executeUpdate();
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		close();
+	}
+
+	return cnt;
+}	public int update5(String 연락처) {
+
+	int cnt = 0;
+	try {
+		getConn();
+
+		String sql = "UPDATE apply SET 연락처=?";
+
+		pst = conn.prepareStatement(sql);
+
+		pst.setString(1, 연락처);
+
+		cnt = pst.executeUpdate();
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		close();
+	}
+
+	return cnt;
+} 
+	//로그인기능
+	public ArrayList<HOST_VO> login(){
+		ArrayList<HOST_VO> HOST = new ArrayList<HOST_VO>();
+		
+		try {
+			getConn();
+			
+			String sql = "SELECT iD, pW FROM HOST_VO where iD=? and pW = ?";
+			
+			pst = conn.prepareStatement(sql);
+			
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				String iD = rs.getString(1);
+				String pW = rs.getString(2);
+		} 
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return HOST;
+		}
 }
