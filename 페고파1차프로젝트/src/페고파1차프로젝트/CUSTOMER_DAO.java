57e204cbcee600bf237d592cde3ed3e8c8e71e1c
SMT071 <SMT071@218.157.111.43>
2020-07-29 11:56:24 +0900

    customer vo, dao 완성

--- 페고파1차프로젝트/src/페고파1차프로젝트/CUSTOMER_DAO.java
+++ 페고파1차프로젝트/src/페고파1차프로젝트/CUSTOMER_DAO.java
@@ -11,38 +11,67 @@ public class CUSTOMER_DAO {
 	Connection conn = null;
 	PreparedStatement pst = null;
 	ResultSet rs = null;
-	
+
 	private void getConn() {
-	String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	String user = "hr";
-	String password = "hr";
-	
-	try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		conn = DriverManager.getConnection(url, user, password);
-	} catch (ClassNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	try {
-		if(rs != null) {
-		rs.close();
-		}if(pst != null) {
-			pst.close();
-		}if(conn!=null) {
-			conn.close();
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "hr";
+		String password = "hr";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
 	}
-//	public ArrayList<CUSTOMER_VO> selectAll(){
-		
+
+	private void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pst != null) {
+				pst.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	
-}
+
+	// 전체선택가져오기
+	public ArrayList<CUSTOMER_VO> selectAll() {
+		ArrayList<CUSTOMER_VO> CUSTOMER = new ArrayList<CUSTOMER_VO>();
+
+		getConn();
+		try {
+			String sql = "select * from CUSTOMER";
+			pst = conn.prepareStatement(sql);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				String ID = rs.getString(1);
+				String PW = rs.getString(2);
+				String 주소 = rs.getString(3);
+				String 이름 = rs.getString(4);
+				String 연락처 = rs.getString(5);
+				String 생년월일 = rs.getString(6);
+				String 이메일 = rs.getString(7);
+				CUSTOMER.add(new CUSTOMER_VO(ID, PW, 주소, 이름, 연락처, 생년월일, 이메일));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+
+		return CUSTOMER;
+	}
 }
--- 페고파1차프로젝트/src/페고파1차프로젝트/CUSTOMER_VO.java
+++ 페고파1차프로젝트/src/페고파1차프로젝트/CUSTOMER_VO.java
@@ -8,4 +8,56 @@ public class CUSTOMER_VO {
 	String 연락처;
 	String 생년월일;
 	String 이메일;
+	
+	public CUSTOMER_VO(String iD, String pW, String 주소, String 이름, String 연락처, String 생년월일, String 이메일) {
+		ID = iD;
+		PW = pW;
+		this.주소 = 주소;
+		this.이름 = 이름;
+		this.연락처 = 연락처;
+		this.생년월일 = 생년월일;
+		this.이메일 = 이메일;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getPW() {
+		return PW;
+	}
+	public void setPW(String pW) {
+		PW = pW;
+	}
+	public String get주소() {
+		return 주소;
+	}
+	public void set주소(String 주소) {
+		this.주소 = 주소;
+	}
+	public String get이름() {
+		return 이름;
+	}
+	public void set이름(String 이름) {
+		this.이름 = 이름;
+	}
+	public String get연락처() {
+		return 연락처;
+	}
+	public void set연락처(String 연락처) {
+		this.연락처 = 연락처;
+	}
+	public String get생년월일() {
+		return 생년월일;
+	}
+	public void set생년월일(String 생년월일) {
+		this.생년월일 = 생년월일;
+	}
+	public String get이메일() {
+		return 이메일;
+	}
+	public void set이메일(String 이메일) {
+		this.이메일 = 이메일;
+	}
 }
