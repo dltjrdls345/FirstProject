0a49c3bab395dfc49255eb003f72a582b753f92a
SMT082 <SMT082@125.136.92.174>
2020-07-30 14:06:22 +0900

    code for uploading img

--- 페고파1차프로젝트/src/페고파1차프로젝트/img.java
+++ 페고파1차프로젝트/src/페고파1차프로젝트/img.java
@@ -13,9 +13,6 @@ public class img {
 	private JFrame frame;
 	Image image1;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -29,13 +26,9 @@ public class img {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public img() {
 		initialize();
 	}
-
 
 	private void initialize() {
 		frame = new JFrame();
@@ -43,10 +36,14 @@ public class img {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		String url = getClass().getResource("").getPath();
-		System.out.println(url);
-
-		image1 = new ImageIcon(url + "1.png").getImage();
+//		절대경로 찾기..?
+//		String url = getClass().getResource("").getPath();
+//		System.out.println(url);
+//		image1 = new ImageIcon(url + "1.png").getImage();
+	
+		
+		String img1 = "C:\\Users\\SMT082\\git\\FirstProject\\페고파1차프로젝트\\image\\1.png";
+		Image image1 = new ImageIcon(img1).getImage();
 		JLabel lbl_img1 = new JLabel(new ImageIcon(image1.getScaledInstance(343, 481, Image.SCALE_SMOOTH)));
 		lbl_img1.setBounds(0, 0, 343, 481);
 		frame.getContentPane().add(lbl_img1);
